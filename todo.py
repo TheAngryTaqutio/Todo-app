8eb4adb5ef6532c671501c59d656f87374d45254
Tyler Clark <tjclark599@gmail.com>
2022-01-26 23:10:02 -0600

    Added csv file to save data, made edits to listbox

--- todo.py
+++ todo.py
@@ -1,33 +1,49 @@
 
 import tkinter as tk
 
-
+with open("todo_list.csv","r") as file:
+    d = dict()
+    
+    for line in file:
+        line = line.strip('\n')
+        (key, val) = line.split(",")
+        d[key] = val
+print(d)
 
 class Todo(tk.Tk):
     def __init__(self):
+        
         super().__init__()
+
+        
         self.window()
+        
 
     def window(self):
-        self.geometry("700x300")
+        self.geometry("700x425")
         self.config(bg="#35478C")
+        
         self.things_todo()
         self.add()
         self.delete()
     
 
-    def things_todo(self):
-        self.things = tk.Listbox(width=100,
-                                 height=20,
-                                 bg="#1355a4",
+    def things_todo(self,dict=d):
+        self.things = tk.Listbox(width=60,
+                                 height=18,
+                                 bg="#afb9e1",
+                                 font=8,
                                  highlightbackground="#2893f3",
-                                 highlightthickness=0)
+                                 highlightthickness=0,
+                                 )
+        for key in d:
+            self.things.insert(tk.END, key)
         
-        self.things.grid(column=0, row=0)
+        self.things.grid(column=0, row=0,padx=(5,0))
 
     def add(self):
         self.add_item = tk.Button(text="Add", width=15, height=11, bg="#7587cb")
-        self.add_item.grid(column=1, row=0, sticky="N")
+        self.add_item.grid(column=1, row=0,padx=20,pady=20, sticky="N")
 
     def delete(self):
         self.delete_item = tk.Button(text= "Delete", width=15, height=11, bg="#7587cb")
