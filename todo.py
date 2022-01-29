6224b2f0ebdb601d1db4034902b43dd17bb345b9
Tyler Clark <tjclark599@gmail.com>
2022-01-29 03:57:33 -0600

    Turned Newtodo into a class

--- todo.py
+++ todo.py
@@ -10,33 +10,35 @@ with open("todo_list.csv", "r") as file:
         d[key] = val
 
 
-def new_todo():
-    new_window = tk.Tk()
-    new_window.title("New Task")
-    new_window.geometry("400x100")
+class Newtodo(tk.Tk):
+    
+    def __init__(self):
+        super().__init__()
+        self.new_window.title("New Task")
+        self.new_window.geometry("400x100")
+    
+        new_task_lbl = tk.Label(text="New Task:")
+        new_task_lbl.grid(column=0, row=0)
 
-    new_task_lbl = tk.Label(new_window, text="New Task:")
-    new_task_lbl.grid(column=0, row=0)
+        new_task = tk.Entry()
+        new_task.grid(column=1, row=0, sticky="W")
 
-    new_task = tk.Entry(new_window)
-    new_task.grid(column=1, row=0, sticky="W")
+        desc = tk.Label(text="Description")
+        desc.grid(column=0, row=1)
 
-    desc = tk.Label(new_window, text="Description")
-    desc.grid(column=0, row=1)
+        desc = tk.Entry(width=40)
+        desc.grid(column=1, row=1, sticky="E")
 
-    desc = tk.Entry(new_window, width=40)
-    desc.grid(column=1, row=1, sticky="E")
+        date_lbl = tk.Label(text="Complete by:")
+        date_lbl.grid(column=0, row=2)
 
-    date_lbl = tk.Label(new_window, text="Complete by:")
-    date_lbl.grid(column=0, row=2)
+        date_entry = tk.Entry()
+        date_entry.grid(column=1, row=2, stick="W")
 
-    date_entry = tk.Entry(new_window)
-    date_entry.grid(column=1, row=2, stick="W")
+        add_entry = tk.Button(text="Add")
+        add_entry.grid(column=1, row=3)
 
-    add_entry = tk.Button(new_window, text="Add")
-    add_entry.grid(column=1, row=3)
-
-    new_window.mainloop()
+        self.new_window.mainloop()
 
 
 class Todo(tk.Tk):
@@ -54,7 +56,7 @@ class Todo(tk.Tk):
         self.delete()
 
     def things_todo(self, dict=d):
-        self.things = tk.Listbox(width=60,
+        things = tk.Listbox(width=60,
                                  height=18,
                                  bg="#afb9e1",
                                  font=8,
@@ -66,12 +68,15 @@ class Todo(tk.Tk):
 
         self.things.grid(column=0, row=0, padx=(5, 0))
 
+    def new_todo(self):
+        window = Newtodo()
+
     def add(self):
-        self.add_item = tk.Button(
-            text="Add", width=15, height=11, bg="#7587cb", command=new_todo)
-        self.add_item.grid(column=1, row=0, padx=20, pady=20, sticky="N")
+        add_item = tk.Button(
+            text="Add", width=15, height=11, bg="#7587cb", command=self.new_todo)
+        add_item.grid(column=1, row=0, padx=20, pady=20, sticky="N")
 
     def delete(self):
-        self.delete_item = tk.Button(
+        delete_item = tk.Button(
             text="Delete", width=15, height=11, bg="#7587cb")
-        self.delete_item.grid(column=1, row=0, sticky="S")
+        delete_item.grid(column=1, row=0, sticky="S")
